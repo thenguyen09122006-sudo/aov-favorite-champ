8ff087af6458ec8e2dfc640bfada5a06cac416d5
thenguyen09122006-sudo <thenguyen09122006@gmail.com>
2026-02-12 17:22:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function upDate(previewPic) {
     let imageDiv = document.getElementById("image");
-    imageDiv.innerHTML = previewPic.alt;
+
     imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
+    imageDiv.innerHTML = previewPic.alt;
 }
 
 function undo() {
     let imageDiv = document.getElementById("image");
-    imageDiv.style.backgroundImage = "url('')";
+
+    imageDiv.style.backgroundImage = "";
     imageDiv.innerHTML = "Hover over an image below to display here.";
 }
